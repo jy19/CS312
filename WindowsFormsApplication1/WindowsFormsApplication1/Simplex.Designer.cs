d6ede74afa6f724140fc3defa2c7cf1d6fc2c577
Jessie Young <j.young184@gmail.com>
2015-03-17 01:16:56 -0600

    finishing lab 6

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Simplex.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Simplex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.solveBtn = new System.Windows.Forms.Button();
+            this.CopperText = new System.Windows.Forms.TextBox();
+            this.SilverText = new System.Windows.Forms.TextBox();
+            this.GoldText = new System.Windows.Forms.TextBox();
+            this.PlatText = new System.Windows.Forms.TextBox();
+            this.CopperLbl = new System.Windows.Forms.Label();
+            this.GoldLbl = new System.Windows.Forms.Label();
+            this.PlatLbl = new System.Windows.Forms.Label();
+            this.SilverLbl = new System.Windows.Forms.Label();
+            this.ResultTxt = new System.Windows.Forms.TextBox();
+            this.ResultLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // solveBtn
+            // 
+            this.solveBtn.Location = new System.Drawing.Point(103, 244);
+            this.solveBtn.Name = "solveBtn";
+            this.solveBtn.Size = new System.Drawing.Size(75, 23);
+            this.solveBtn.TabIndex = 0;
+            this.solveBtn.Text = "Solve";
+            this.solveBtn.UseVisualStyleBackColor = true;
+            this.solveBtn.Click += new System.EventHandler(this.solveBtn_Click);
+            // 
+            // CopperText
+            // 
+            this.CopperText.Location = new System.Drawing.Point(121, 32);
+            this.CopperText.Name = "CopperText";
+            this.CopperText.Size = new System.Drawing.Size(104, 20);
+            this.CopperText.TabIndex = 1;
+            // 
+            // SilverText
+            // 
+            this.SilverText.Location = new System.Drawing.Point(121, 84);
+            this.SilverText.Name = "SilverText";
+            this.SilverText.Size = new System.Drawing.Size(104, 20);
+            this.SilverText.TabIndex = 2;
+            // 
+            // GoldText
+            // 
+            this.GoldText.Location = new System.Drawing.Point(121, 58);
+            this.GoldText.Name = "GoldText";
+            this.GoldText.Size = new System.Drawing.Size(104, 20);
+            this.GoldText.TabIndex = 3;
+            // 
+            // PlatText
+            // 
+            this.PlatText.Location = new System.Drawing.Point(121, 110);
+            this.PlatText.Name = "PlatText";
+            this.PlatText.Size = new System.Drawing.Size(104, 20);
+            this.PlatText.TabIndex = 4;
+            // 
+            // CopperLbl
+            // 
+            this.CopperLbl.AutoSize = true;
+            this.CopperLbl.Location = new System.Drawing.Point(62, 39);
+            this.CopperLbl.Name = "CopperLbl";
+            this.CopperLbl.Size = new System.Drawing.Size(41, 13);
+            this.CopperLbl.TabIndex = 5;
+            this.CopperLbl.Text = "Copper";
+            // 
+            // GoldLbl
+            // 
+            this.GoldLbl.AutoSize = true;
+            this.GoldLbl.Location = new System.Drawing.Point(62, 61);
+            this.GoldLbl.Name = "GoldLbl";
+            this.GoldLbl.Size = new System.Drawing.Size(29, 13);
+            this.GoldLbl.TabIndex = 6;
+            this.GoldLbl.Text = "Gold";
+            this.GoldLbl.UseWaitCursor = true;
+            // 
+            // PlatLbl
+            // 
+            this.PlatLbl.AutoSize = true;
+            this.PlatLbl.Location = new System.Drawing.Point(62, 113);
+            this.PlatLbl.Name = "PlatLbl";
+            this.PlatLbl.Size = new System.Drawing.Size(47, 13);
+            this.PlatLbl.TabIndex = 7;
+            this.PlatLbl.Text = "Platinum";
+            // 
+            // SilverLbl
+            // 
+            this.SilverLbl.AutoSize = true;
+            this.SilverLbl.Location = new System.Drawing.Point(62, 87);
+            this.SilverLbl.Name = "SilverLbl";
+            this.SilverLbl.Size = new System.Drawing.Size(33, 13);
+            this.SilverLbl.TabIndex = 8;
+            this.SilverLbl.Text = "Silver";
+            // 
+            // ResultTxt
+            // 
+            this.ResultTxt.Location = new System.Drawing.Point(121, 136);
+            this.ResultTxt.Name = "ResultTxt";
+            this.ResultTxt.Size = new System.Drawing.Size(104, 20);
+            this.ResultTxt.TabIndex = 9;
+            // 
+            // ResultLbl
+            // 
+            this.ResultLbl.AutoSize = true;
+            this.ResultLbl.Location = new System.Drawing.Point(62, 139);
+            this.ResultLbl.Name = "ResultLbl";
+            this.ResultLbl.Size = new System.Drawing.Size(37, 13);
+            this.ResultLbl.TabIndex = 10;
+            this.ResultLbl.Text = "Result";
+            // 
+            // Simplex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 279);
+            this.Controls.Add(this.ResultLbl);
+            this.Controls.Add(this.ResultTxt);
+            this.Controls.Add(this.SilverLbl);
+            this.Controls.Add(this.PlatLbl);
+            this.Controls.Add(this.GoldLbl);
+            this.Controls.Add(this.CopperLbl);
+            this.Controls.Add(this.PlatText);
+            this.Controls.Add(this.GoldText);
+            this.Controls.Add(this.SilverText);
+            this.Controls.Add(this.CopperText);
+            this.Controls.Add(this.solveBtn);
+            this.Name = "Simplex";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button solveBtn;
+        private System.Windows.Forms.TextBox CopperText;
+        private System.Windows.Forms.TextBox SilverText;
+        private System.Windows.Forms.TextBox GoldText;
+        private System.Windows.Forms.TextBox PlatText;
+        private System.Windows.Forms.Label CopperLbl;
+        private System.Windows.Forms.Label GoldLbl;
+        private System.Windows.Forms.Label PlatLbl;
+        private System.Windows.Forms.Label SilverLbl;
+        private System.Windows.Forms.TextBox ResultTxt;
+        private System.Windows.Forms.Label ResultLbl;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Simplex.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Simplex : Form
+    {
+        public Simplex()
+        {
+            InitializeComponent();
+        }
+
+        private void solveBtn_Click(object sender, EventArgs e)
+        {
+            initialize();
+            solvewSimplex();
+            CopperText.Text = b[0].ToString();
+            GoldText.Text = b[1].ToString();
+            SilverText.Text = b[2].ToString();
+            PlatText.Text = b[3].ToString();
+            ResultTxt.Text = v.ToString();
+        }
+
+        //private int[] N; //positions of non-basic variables
+        private List<int> N;
+        //private int[] B; //positions of basic variables
+        private List<int> B;
+        private double[,] A; //padded matrix
+        private double[] b; //solutions
+        private double[] c; //objective function
+        private double v = 0; //result
+
+        private Tuple<double[], double> solvewSimplex()
+        {
+            int e; //e is the entering var
+            int l; //l is the leaving var
+            do{
+                //if there exists an e in N such that c_e > 0
+                if (getPosE() != -1)
+                {
+                    e = getPosE();
+                    double dMin = double.MaxValue; //dMin is set to infinite
+                    l = -1;
+                    foreach (int index in B)
+                    {
+                        if (A[index, e] > 0)
+                        {
+                            double d = b[index] / A[index, e]; //delta is check ratio for current constraint
+                            if(d < dMin) {
+                                //update the min
+                                dMin = d;
+                                //update the leaving var index
+                                l = index;
+                            }
+                        }
+                    }
+                    //l in B is now index of smallest check ratio : dMin
+                    if(dMin == double.MaxValue) {
+                        //unbounded
+                        //return something...(currently just returning an empty array + 'infinite')
+                        return Tuple.Create(new double[0], double.MaxValue);
+                    }
+                    else
+                    {
+                        //else pivot with new entering var
+                        pivot(e, l);
+                    }
+                }
+                else
+                {
+                    e = -1;
+                }
+            } while(e != -1); //while there are still entering variables
+            
+            //set non-basic variables to 0 and everything else to optimal solution
+            for (int i = 0; i < b.Length; i++ )
+            {   
+                //if i is in B
+                if(B.Contains(i)) {
+                    c[i] = b[i];
+                }
+                else
+                {
+                    c[i] = 0;
+                }
+            }
+
+            return Tuple.Create(c, v);
+        }
+
+        //e is entering var
+        //l is leaving var
+        private void pivot(int e, int l)
+        {
+            //compute coefficients for contraint for new basic var x_e
+            b[e] = b[l]/A[l, e];
+
+            //for each j in N besides e
+            foreach (int j in N)
+            {
+                if(j == e) {
+                    continue; //skip e
+                }
+                A[e, j] = A[l, j] / A[l, e];
+            }
+
+            A[e, l] = 1 / A[l, e];
+            //compute the new coefficients for the other constraints
+            //for each i in B besidse l
+            foreach (int i in B)
+            {
+                if(i == l) {
+                    continue; //skip l
+                }
+                b[i] = b[i] - (A[i, e] * b[e]);
+                foreach (int j in N)
+                {
+                    if(j == e) {
+                        continue;
+                    }
+                    A[i, j] = A[i, j] - (A[i, e] * A[e, j]);
+                }
+                A[i, l] = (A[i, e] * A[e, l]) * -1;
+            }
+
+            //compute the new objective function
+            v = v + (c[e] * b[e]);
+            foreach (int j in N)
+            {
+                if(j == e) {
+                    continue;
+                }
+                c[j] = c[j] - (c[e] * A[e, j]);
+            }
+
+            c[l] = (c[e]*A[e, l])*-1; 
+
+            //compute new basic and non-basic variable index sets
+            N.Remove(e);
+            N.Add(l);
+
+            B.Remove(l);
+            B.Add(e);
+
+        }
+
+        private void initialize()
+        {
+            //initialize the padded matrix with constraints
+            A = new double[9, 9] 
+            {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0.1, 0.5, 0.333, 1, 0, 0, 0, 0, 0},
+                {30, 15, 19, 12, 0, 0, 0, 0, 0},
+                {1000, 6000, 4100, 9100, 0, 0, 0, 0, 0},
+                {50, 20, 21, 10, 0, 0, 0, 0, 0},
+                {4, 6, 19, 30, 0, 0, 0, 0, 0}
+            };
+
+            //initialize c
+            c = new double[9] { 10.2, 422.3, 6.91, 853, 0, 0, 0, 0, 0};
+
+            //initialize b
+            b = new double[9] { 0, 0, 0, 0, 2000, 1000, 1000000, 640, 432};
+
+            //initialize where the basic and non-basic vars are
+            //N = new int[4] { 0, 1, 2, 3 };
+            //B = new int[5] { 4, 5, 6, 7, 8};
+            N = new List<int>(new int[] { 0, 1, 2, 3});
+            B = new List<int>(new int[] { 4, 5, 6, 7, 8 });
+        }
+
+        //gets the index of positive value in N for e
+        private int getPosE()
+        {
+            foreach (int index in N)
+            {
+                if (c[index] > 0) 
+                    return index;
+            }
+            return -1; //if no positive value found in N
+        }
+
+    }
+}
